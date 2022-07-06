6e04e59699e3ec0ba0a162769fc15c1dd097f5a4
Mark <i.love.rock182@gmail.com>
2022-07-06 13:24:38 +0300

    final

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,12 +1,11 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 import java.sql.SQLException;
-import java.util.List;
 
 public class MainApp {
    public static void main(String[] args) throws SQLException {
@@ -15,19 +14,26 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
+      Car mazda = new Car("Mazda", 323);
+      Car toyota = new Car("Toyota", 4);
+      Car mitsubishi = new Car("Mitsubishi", 3000);
+      Car lexus = new Car("Lexus", 400);
+
+      userService.add(user1.setCar(mazda).setUser(user1));
+      userService.add(user2.setCar(toyota).setUser(user2));
+      userService.add(user3.setCar(mitsubishi).setUser(user3));
+      userService.add(user4.setCar(lexus).setUser(user4));
+
+      for (User user : userService.listUsers()) {
+         System.out.println(user.toString() + " " + user.getCar());
       }
+
+      System.out.println(userService.getUserByCar("Mazda", 323));
 
       context.close();
    }
